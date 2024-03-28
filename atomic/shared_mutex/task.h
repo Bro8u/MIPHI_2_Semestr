7cafae73810d069a2d7d193479b303da93d1c989
Bro8u <betttttt@yandex.ru>
2024-03-28 17:09:47 +0300

    atomic

--- atomic/shared_mutex/task.h
+++ atomic/shared_mutex/task.h
@@ -1,30 +1,11 @@
-#pragma once
 #include <atomic>
-
-class Mutex {
-public:
-    Mutex() : locked(false) {}
-
-    void lock() {
-        while (locked.exchange(true, std::memory_order_acquire)) {}
-    }
-
-    void unlock() {
-        locked.store(false, std::memory_order_release);
-    }
-
-private:
-    std::atomic<bool> locked;
-};
-
 class SharedMutex {
 public:
     SharedMutex(): write_lock(false), reader_count(0) {}
+    
     void lock() {
-        mtx.lock();
-        while (reader_count.load()){}
-        while (write_lock.exchange(true, std::memory_order_acquire)){}
-        mtx.unlock();
+        while (write_lock.exchange(true, std::memory_order_acquire)) {}
+        while (reader_count.load() != 0) {} 
     }
 
     void unlock() {
@@ -32,19 +13,21 @@ public:
     }
 
     void lock_shared() {
-        mtx.lock();
-        reader_count.fetch_add(1);
-        mtx.unlock();
+        while (true) {
+            while (write_lock.load()) {} 
+            reader_count.fetch_add(1);
+            if (!write_lock.load()){
+                break; 
+            }
+            reader_count.fetch_sub(1);
+        }
     }
 
     void unlock_shared() {
-        mtx.lock();
         reader_count.fetch_sub(1);
-        mtx.unlock();
-        
     }
+
 private:
     std::atomic<bool> write_lock;
     std::atomic<int> reader_count;
-    Mutex mtx;
 };
