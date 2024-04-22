4165ed5a4315fd73f721e5b3ed459822a956a11b
Bro8u <betttttt@yandex.ru>
2024-04-22 13:50:15 +0300

    part 3

--- project-part-3/socket-connect/tcp_connect.cpp
+++ project-part-3/socket-connect/tcp_connect.cpp
@@ -15,11 +15,12 @@
 #include <cstring>
 #include <iostream>
 
-
 TcpConnect::TcpConnect(std::string ip, int port, std::chrono::milliseconds connectTimeout, std::chrono::milliseconds readTimeout):
-    ip_(ip), port_(port),connectTimeout_(connectTimeout), readTimeout_(readTimeout){}
+        ip_(ip), port_(port), connectTimeout_(connectTimeout), readTimeout_(readTimeout) {}
 
-TcpConnect::~TcpConnect() {}
+TcpConnect::~TcpConnect() {
+    CloseConnection();
+}
 
 void TcpConnect::EstablishConnection() {
     sock_ = socket(AF_INET, SOCK_STREAM, 0);
@@ -28,36 +29,36 @@ void TcpConnect::EstablishConnection() {
     }
 
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr)); // Обнуление структуры
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port_);
     if (inet_pton(AF_INET, ip_.c_str(), &serv_addr.sin_addr) <= 0) {
+        close(sock_);  // Clean up the socket before throwing
         throw std::runtime_error("Неверный адрес IP");
     }
 
-
-    struct timeval tv;
-    tv.tv_sec = connectTimeout_.count() / 1000;
-    tv.tv_usec = (connectTimeout_.count() % 1000) * 1000;
-    setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
-
     if (connect(sock_, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        close(sock_);  // Clean up the socket before throwing
         throw std::runtime_error("Не удалось установить соединение");
     }
 }
 
 void TcpConnect::SendData(const std::string& data) const {
-    send(sock_, data.c_str(), data.size(), 0);
+    if (send(sock_, data.c_str(), data.size(), 0) < 0) {
+        throw std::runtime_error("Ошибка при отправке данных");
+    }
 }
 
 std::string TcpConnect::ReceiveData(size_t bufferSize) const {
     if (bufferSize) {
         std::vector<char> buffer(bufferSize);
         ssize_t bytes_received = recv(sock_, buffer.data(), bufferSize, 0);
-        if (bytes_received > 0) {
-            return std::string(buffer.data(), bytes_received);
+        if (bytes_received < 0) {
+            throw std::runtime_error("Ошибка при получении данных");
+        } else if (bytes_received == 0) {
+            throw std::runtime_error("Соединение закрыто");
         }
-        return "";
+        return std::string(buffer.data(), bytes_received);
     }
 
     char lengthBuffer[4];
@@ -68,14 +69,19 @@ std::string TcpConnect::ReceiveData(size_t bufferSize) const {
     int length = BytesToInt(lengthBuffer);
     std::vector<char> res(length);
     bytes_received = recv(sock_, res.data(), length, 0);
-    if (bytes_received > 0) {
-        return std::string(res.data(), bytes_received);
+    if (bytes_received < 0) {
+        throw std::runtime_error("Ошибка при получении данных");
+    } else if (bytes_received == 0) {
+        throw std::runtime_error("Соединение закрыто");
     }
-    return "";
+    return std::string(res.data(), bytes_received);
 }
 
 void TcpConnect::CloseConnection() {
-    close(sock_);
+    if (sock_ != -1) {
+        close(sock_);
+        sock_ = -1;  // Invalidate the socket descriptor after closing
+    }
 }
 
 const std::string &TcpConnect::GetIp() const {
